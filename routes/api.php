4656f83e456fcab83ec1a370ddf71811a267a9bf
Usenmfon <usenmfonuko@gmail.com>
2025-11-05 13:32:16 +0100

    update: routing

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,6 @@ use App\Http\Controllers\DevAssistController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::match(['get', 'post'], '/dev_assit/webhook', [DevAssistController::class, 'handleWebhook']);
+Route::match(['get', 'post'], '/a2a/agent/dev_assist', [DevAssistController::class, 'handleWebhook']);
 Route::get('/logs', [DevAssistController::class, 'logs']);
 
